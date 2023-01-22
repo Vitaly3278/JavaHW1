b7dc65be5f2659c34d14ed9f60cd81cb134c0e3d
Vitaly Osipov <vital-nvl@mail.ru>
2023-01-22 14:35:38 +0300

    work3

--- work3.java
+++ work3.java
@@ -9,8 +9,8 @@ package HW1;
 public class work3 {
     public static void main(String[] args) {
         int[] arr = {3, 2, 2, 3};
-        int K = 3;
-        int[] ans = moveElement(arr, K);
+        int val = 3;
+        int[] ans = moveElement(arr, val);
 
         for (int i = 0; i < arr.length; i++)
             System.out.print(ans[i] + " ");
